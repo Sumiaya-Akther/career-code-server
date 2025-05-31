3b68d44cc20ba86e227cc198819f2319d43e453f
Sumiaya Akther <sumiayaakther15@gmail.com>
2025-05-31 23:40:20 +0600

    new

--- index.js
+++ index.js
@@ -10,10 +10,6 @@
  app.use(express.json());
 
 
-
-// career-code
-// MHpI3poAls8woAex
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mdnalzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
